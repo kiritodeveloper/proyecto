c9d18b88e71b253ffdbf67ea7ce5e880b13c5a56
Abel Chils <abelcht@outlook.es>
2019-02-28 16:59:28 +0100

    improved

--- RobotDrawer.py
+++ RobotDrawer.py
@@ -1,12 +1,12 @@
 import time
-from multiprocessing import Value, Process, Queue
+from multiprocessing import Process
 
 from plot_robot import dibrobot
 import matplotlib.pyplot as plt
 
 
-def start_robot_drawer(finished: Value, period: float, child_conn: Queue):
-    p: Process = Process(target=loop_robot_drawer, args=(finished, period, child_conn))
+def start_robot_drawer(finished, robot):
+    p: Process = Process(target=loop_robot_drawer, args=(finished, robot))
     p.start()
 
     # Time to start drawer
@@ -14,16 +14,14 @@ def start_robot_drawer(finished: Value, period: float, child_conn: Queue):
     time.sleep(10)
 
 
-def loop_robot_drawer(finished: Value, period: float, child_conn: Queue):
+def loop_robot_drawer(finished, robot):
     print("Drawer started")
     dibrobot([0, 0, 0], 'r', 'p')
     plt.ion()
 
     time.sleep(10)
 
-    while not finished.value or not child_conn.empty():
-        [x, y, th] = child_conn.get()
+    while not finished.value:
+        [x, y, th] = robot.readOdometry()
         dibrobot([x, y, th], 'r', 'p')
-        plt.pause(period)
-
-    child_conn.close()
+        plt.pause(0.3)
