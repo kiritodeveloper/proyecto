3ed182cb8d34c245f052372f6f4d5db541b15b0d
Carlos Marañes <carlosmn1997@gmail.com>
2019-03-19 19:37:40 +0100

    basketttt

--- RobotFrameCapturer.py
+++ RobotFrameCapturer.py
@@ -141,7 +141,7 @@ class RobotFrameCapturer(object):
             center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
 
             # only proceed if the radius meets a minimum size
-            if radius > 10:
+            if radius > 5:
                 # draw the circle and centroid on the frame,
                 # then update the list of tracked points
                 cv2.circle(imgBGR, (int(x), int(y)), int(radius),
