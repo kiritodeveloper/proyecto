3c8fb2abaeb3347d4700b8b1f1337c0ba8c521c7
Carlos Marañes <carlosmn1997@gmail.com>
2019-04-03 18:06:19 +0200

    depurando sonar

--- MapLib.py
+++ MapLib.py
@@ -507,8 +507,8 @@ class Map2D:
         y = odometry[1]
         th = odometry[2]
         print('Convierto: ', x, y)
-        x = (x - 0.2) // self.sizeCell
-        y = (y - 0.2) // self.sizeCell
+        x = (x - 0.2) // (self.sizeCell/100)
+        y = (y - 0.2) // (self.sizeCell/100)
 
         return [x, y, th]
 
