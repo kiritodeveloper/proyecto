a8b85541aac8ec9793c8fd9c5e1b12475427a9ed
Carlos Marañes <carlosmn1997@gmail.com>
2019-04-05 13:18:07 +0200

    entrega T4

--- plot_log.py
+++ plot_log.py
@@ -6,7 +6,7 @@ def main():
     Plot log file
     """
     #plot_log("./path_logs/trayectoria_2.csv")
-    plot_log_with_map('./out/trayectoria_1.csv', './maps/mapa3.txt')
+    plot_log_with_map('./out/trayectoria_entrega.csv', './maps/mapa_debug.txt')
 
 if __name__ == "__main__":
     main()
