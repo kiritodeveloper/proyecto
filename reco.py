e1eb1f8b2ac38daa2a73962523aaaeb0b79006cf
Carlos Marañes <carlosmn1997@gmail.com>
2019-04-11 17:59:57 +0200

    probando por rangos

--- reco.py
+++ reco.py
@@ -240,8 +240,8 @@ class Reco:
                 frame = self.rawCapture.array
 
                 #frame = cv2.flip(frame, -1)  # to rotate 180
-                #cv2.imshow("Lo que veo",frame)
-                #cv2.waitKey(0)
+                cv2.imshow("Lo que veo",frame)
+                cv2.waitKey(0)
                 if DEBUG > 2:
                     cv2.imshow("Current view", frame)
                     cv2.imshow("Current target", imReference)
