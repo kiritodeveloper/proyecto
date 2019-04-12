df727ddbc525ef903517823253bbf346d203827d
Abel Chils <abelcht@outlook.es>
2019-04-12 09:49:29 +0200

    arreglando odometria en recto

--- p4_base.py
+++ p4_base.py
@@ -110,6 +110,6 @@ if __name__ == "__main__":
     # Add as many args as you need ...
     parser = argparse.ArgumentParser()
     parser.add_argument("-m", "--mapfile", help="path to find map file",
-                        default="./maps/mapa3.txt")
+                        default="./maps/mapa_debug.txt")
     args = parser.parse_args()
     main(args)
