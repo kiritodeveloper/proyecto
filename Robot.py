faa5dde76c803857ceb5336f8caa08a6a2dc7d98
Carlos Marañes <carlosmn1997@gmail.com>
2019-05-09 17:51:30 +0200

    entregando el trabajo

--- Robot.py
+++ Robot.py
@@ -552,12 +552,12 @@ class Robot:
         if movement != self.basket_state:
             if movement == 'up':
                 self.BP.set_motor_dps(self.motor_port_basket, -270)
-                time.sleep(0.3)
+                time.sleep(0.27)
                 self.BP.set_motor_dps(self.motor_port_basket, 0)
                 self.basket_state = 'up'
             elif movement == 'down':
                 self.BP.set_motor_dps(self.motor_port_basket, 270)
-                time.sleep(0.3)
+                time.sleep(0.27)
                 self.BP.set_motor_dps(self.motor_port_basket, 0)
                 self.basket_state = 'down'
 
