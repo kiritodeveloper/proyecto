8af571f39e0bd54f8e99591a115e09c437432c85
Carlos Marañes <carlosmn1997@gmail.com>
2019-05-13 12:22:15 +0200

    depurando todo

--- trabajo.py
+++ trabajo.py
@@ -188,12 +188,12 @@ def main(args):
                 starting_point = coord2Meters((1, 3, -math.pi / 2))
                 init_pos = [1, 3]
                 goal_x = 3
-                goal_y = 2
+                goal_y = 3
             else:  # Salida es B
                 starting_point = coord2Meters((5, 3, -math.pi / 2))
                 init_pos = [5, 3]
                 goal_x = 3
-                goal_y = 2
+                goal_y = 3
 
             if primera:
                 robot = Robot(starting_point)
@@ -251,10 +251,10 @@ def main(args):
 
             # ORIENTARSE Y AVANZAR UN POCO PARA DELANTE
             # Avanza un poco hacia delante para cruzar la linea de meta
-            robot.orientate(math.pi / 2)
-            robot.setSpeed(0.3, 0)
-            time.sleep(2)
-            robot.setSpeed(0, 0)
+            #robot.orientate(math.pi / 2)
+            #robot.setSpeed(0.3, 0)
+            #time.sleep(2)
+            #robot.setSpeed(0, 0)
 
             [x, y, th] = robot.readOdometry()
             print("Estoy principio 4", x, y, th)
