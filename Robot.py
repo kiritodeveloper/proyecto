08d544e4727dfe65aaedcfefb4dbd7ca26e2747d
Carlos Marañes <carlosmn1997@gmail.com>
2019-05-13 11:18:57 +0200

    depurando todo

--- Robot.py
+++ Robot.py
@@ -614,7 +614,7 @@ class Robot:
                 last_error = actual_error
                 while last_error >= actual_error:
                     [x_odo, y_odo, _] = self.readOdometry()
-                    print(x_odo, y_odo)
+                    print(x_odo, y_odo, x, y)
                     last_error = actual_error
                     actual_error = math.sqrt((x_odo - x) ** 2 + (y_odo - y) ** 2)
                     t_next_period += self.P
