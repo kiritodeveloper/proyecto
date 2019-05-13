a3d70135e13c0d11236d7d7ee8542ae97a5e673c
Carlos Marañes <carlosmn1997@gmail.com>
2019-05-13 12:59:48 +0200

    depurando todo

--- Robot.py
+++ Robot.py
@@ -670,7 +670,7 @@ class Robot:
         print('Estoy buscando th ', aligned_angle)
         print('Velocidad ', turn_speed)
         self.setSpeed(0, turn_speed)
-        self.wait_for_th(aligned_angle, 0.10)
+        self.wait_for_th(aligned_angle, 0.2)
 
         correction_speed = np.sign(turn_speed) * math.pi / 8
 
